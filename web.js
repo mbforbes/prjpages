c411510e64fe15ad8300cdf7c8d08c4a9abd1ccd
Maxwell Forbes <m.b.forbes@gmail.com>
2014-01-21 00:08:45 -0800

    Add in HTML templates I'd made before; using Jade as templating engine, add bootstrap as layout frontend.

--- web.js
+++ web.js
@@ -1,13 +1,23 @@
+/**
+ * Module dependencies
+ */
 var express = require("express");
+// var jade = require("jade");
+
+var pub = __dirname + '/public';
 var app = express();
+
 app.use(express.logger());
+app.use(express.static(pub));
 
 app.get('/', function(request, response) {
-  response.send('Num 1.');
+	response.sendfile(pub + '/pt_prj.html');
 });
 
-app.get('/2', function(request, response) {
-  response.send('Num 2.');
+app.get('/jadetest', function(request, response) {
+	response.render(pub + '/testpage.jade');
+	// var options = {pretty: true, deubg: true}
+	// response.send(jade.renderFile(pub + '/testpage.jade', options))
 });
 
 var port = process.env.PORT || 5000;
