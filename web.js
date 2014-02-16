9874d2c5b6de69afc1fe7151fff6c2d98e133f47
Maxwell Forbes <m.b.forbes@gmail.com>
2014-02-15 16:12:33 -0800

    cleanup by removing node_modules, also pre-commit before removing a bunch of express calls so that they're saved in git

--- web.js
+++ web.js
@@ -2,20 +2,34 @@
  * Module dependencies
  */
 var express = require("express");
+var fs = require("fs");
+var merge = require('merge')
 // var jade = require("jade");
 
+var jade_options = {pretty: true};
 var pub = __dirname + '/public';
 var app = express();
+
+// data
+var content = JSON.parse(fs.readFileSync(pub + "/testobj.json", "utf8"));
+var fs.readdirSync('')
 
 app.use(express.logger());
 app.use(express.static(pub));
 
 app.get('/', function(request, response) {
-	response.sendfile(pub + '/pt_prj.html');
+	// response.sendfile(pub + '/pt_prj.html');
+	locals = {content: content}
+	response.render(pub + '/pt_prj.jade', merge(jade_options, locals));	
 });
 
-app.get('/jadetest', function(request, response) {
+app.get('/jadetest1', function(request, response) {
 	response.render(pub + '/testpage.jade');
+	// var options = {pretty: true, deubg: true}
+	// response.send(jade.renderFile(pub + '/testpage.jade', options))
+});
+
+app.get('/jadetest2', function(request, response) {
 	// var options = {pretty: true, deubg: true}
 	// response.send(jade.renderFile(pub + '/testpage.jade', options))
 });
