db9741bce32addf20b4a81004f59980033c56ddb
Maxwell Forbes <m.b.forbes@gmail.com>
2014-02-15 16:15:19 -0800

    Remove unused code

--- web.js
+++ web.js
@@ -18,20 +18,8 @@ app.use(express.logger());
 app.use(express.static(pub));
 
 app.get('/', function(request, response) {
-	// response.sendfile(pub + '/pt_prj.html');
 	locals = {content: content}
 	response.render(pub + '/pt_prj.jade', merge(jade_options, locals));	
-});
-
-app.get('/jadetest1', function(request, response) {
-	response.render(pub + '/testpage.jade');
-	// var options = {pretty: true, deubg: true}
-	// response.send(jade.renderFile(pub + '/testpage.jade', options))
-});
-
-app.get('/jadetest2', function(request, response) {
-	// var options = {pretty: true, deubg: true}
-	// response.send(jade.renderFile(pub + '/testpage.jade', options))
 });
 
 var port = process.env.PORT || 5000;
